e8f453e725d8d0674fa6f323c73dd711f4387908
Armando Restrepo <AresDev@Armandos-MacBook-Pro.local>
2015-04-22 22:25:32 -0500

    Add files

---
+++ splitwithtabbar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  splitwithtabbar
+//
+//  Created by Armando Restrepo on 4/22/15.
+//  Copyright (c) 2015 AresDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
